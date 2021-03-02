5db4d9f49ae0468ec4c4c346353f5a6ca28c4625
teddy0818 <taddy0818@naver.com>
2021-03-02 20:00:40 +0900

    커뮤니티 추가

---
+++ src/main/java/com/koreait/ohouse/CommunityController.java
@@ -0,0 +1,11 @@
+package com.koreait.ohouse;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class CommunityController {
+
+}
