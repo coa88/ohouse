409709aaf36c738ce1f071592abe7d7e48c36ba0
teddy0818 <taddy0818@naver.com>
2021-03-04 20:20:10 +0900

    회원가입 작업중

--- src/main/java/com/koreait/ohouse/user/UserService.java
+++ src/main/java/com/koreait/ohouse/user/UserService.java
@@ -7,25 +7,27 @@ import org.springframework.stereotype.Service;
 
 import com.koreait.ohouse.model.UserEntity;
 
-
-
 @Service
 public class UserService {
 	@Autowired
 	private UserMapper mapper;
-	
+
 	@Autowired
 	private HttpSession hs;
 
 	public int insUser(UserEntity p) {
+		System.out.println("backEmail : " + hs.getAttribute(""));
+//		String emailId = (hs.getAttribute("email_id") + hs.getAttribute("email"));
+//		p.setEmailId(emailId);
 		return mapper.insUser(p);
 	}
+
 	public UserEntity selUser(UserEntity p) {
 		return mapper.selUser(p);
 	}
+
 	public int delUser(UserEntity p) {
 		return mapper.delUser(p);
 	}
-	
-	
+
 }
