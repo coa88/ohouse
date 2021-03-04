2d6f9d33c4e6b8cde7ab351b9b8a00e62d8dc7f8
teddy0818 <taddy0818@naver.com>
2021-03-04 20:31:04 +0900

    오류 수정 및 회원가입 작업중

--- src/main/java/com/koreait/ohouse/store/StoreController.java
+++ src/main/java/com/koreait/ohouse/store/StoreController.java
@@ -1,26 +1,6 @@
 package com.koreait.ohouse.store;
 
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/store")
-public class StoreController {
-	
-	@GetMapping("/store_home")
-	public void storeHome() {}
-
-	@GetMapping("/category")
-	public void category() {}
-	
-	@GetMapping("/store_detail")
-	public void store_detail() {}
-
-	@GetMapping("/best")
-	public void best() {}
-=======
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,29 +15,30 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/store")
 public class StoreController {
-	
+
 	final StoreService service;
-	
+
 	@GetMapping("/store_home")
-	public void storeHome() {}
+	public void storeHome() {
+	}
 
 	@GetMapping("/category")
-	public void category() {}
-	
+	public void category() {
+	}
+
 	@PostMapping("/category")
 	public String category(CommunityEntity p) {
 		return "";
 	}
-	
+
 	@GetMapping("/store_detail")
-	public void store_detail() {}
+	public void store_detail() {
+	}
 
 	@GetMapping("/best")
 	public void best(Model model, StoreEntity p) {
 		p.setiProduct(2);
 		model.addAttribute("data", service.selPdBoard(p));
 	}
->>>>>>> branch 'master' of https://github.com/coa88/ohouse.git
-
 
 }
--- src/main/java/com/koreait/ohouse/user/UserMapper.java
+++ src/main/java/com/koreait/ohouse/user/UserMapper.java
@@ -6,20 +6,12 @@ import com.koreait.ohouse.model.UserEntity;
 
 @Mapper
 public interface UserMapper {
-<<<<<<< HEAD
- 
+
 	int insUser(UserEntity p);
+
 	UserEntity selUser(UserEntity p);
+
 	UserEntity updUser(UserEntity p);
-	int delUser(UserEntity p);
-=======
->>>>>>> branch 'master' of https://github.com/coa88/ohouse.git
-
-	int insUser(UserEntity p);
-
-	UserEntity selUser(UserEntity p);
-
-	int updUser(UserEntity p);
 
 	int delUser(UserEntity p);
 
