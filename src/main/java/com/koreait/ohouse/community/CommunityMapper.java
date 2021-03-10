d53ef0423c6c13b0f18eca3d80f526c9bbe031a0
Administrator <Administrator@DESKTOP-FU2AS1G>
2021-03-10 20:00:35 +0900

    작업중

--- src/main/java/com/koreait/ohouse/community/CommunityMapper.java
+++ src/main/java/com/koreait/ohouse/community/CommunityMapper.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.koreait.ohouse.model.CommunityDTO;
 import com.koreait.ohouse.model.CommunityEntity;
 
 @Mapper
 public interface CommunityMapper {
-	int insBoard(CommunityEntity param);
+	int insBoard(CommunityDTO param);
 
 	List<CommunityEntity> selBoardList(CommunityEntity param);
 
